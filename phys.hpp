6dc25cc7ce1ea9c88cd5f94e9e8003241cf809ee
Hakuyume <Hakuyume@users.noreply.github.com>
2014-10-10 09:49:15 +0900

    加速度はプライベートでいいでしょう

--- phys.hpp
+++ phys.hpp
@@ -9,8 +9,10 @@
 
 class Particle
 {
+private:
+  Eigen::Vector3d a;
 public:
-  Eigen::Vector3d p, v, a;
+  Eigen::Vector3d p, v;
   void update_force(void);
   void update_position(double dt);
 };
