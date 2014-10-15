5127dd2a057cd5b7efbadac5c847d888950fff7a
Hakuyume <Hakuyume@users.noreply.github.com>
2014-10-15 15:31:59 +0900

    Cubeオブジェクトに描画を任せる

--- render.cpp
+++ render.cpp
@@ -1,5 +1,4 @@
 #include <GL/glut.h>
-#include <set>
 #include "render.hpp"
 
 #define MC_SIZE 0.5
@@ -7,6 +6,14 @@
 struct CompVector
 {
   bool operator()(const Eigen::Vector3i& a, const Eigen::Vector3i& b);
+};
+
+class Cube
+{
+public:
+  bool m[2][2][2];
+  Cube(void);
+  void draw(void);
 };
 
 bool CompVector::operator()(const Eigen::Vector3i& a, const Eigen::Vector3i& b)
@@ -19,9 +26,22 @@ bool CompVector::operator()(const Eigen::Vector3i& a, const Eigen::Vector3i& b)
   return false;
 }
 
+Cube::Cube(void)
+{
+  for (int x = 0; x <= 1; x++)
+    for (int y = 0; y <= 1; y++)
+      for (int z = 0; z <= 1; z++)
+	m[x][y][z] = false;
+}
+
+void Cube::draw(void)
+{
+  glutSolidCube(MC_SIZE);
+}
+
 void render_particles(const Space& space)
 {
-  std::set<Eigen::Vector3i, CompVector> points;
+  std::map<Eigen::Vector3i, Cube, CompVector> cubes;
 
   for (auto& pt : space.particles)
     for (int x = 0; x <= 1; x++)
@@ -32,16 +52,25 @@ void render_particles(const Space& space)
 	  p(1) = floor(pt.pos(1) / MC_SIZE) + y;
 	  p(2) = floor(pt.pos(2) / MC_SIZE) + z;
 
-	  points.insert(p);
+	  auto iter = cubes.find(p);
+	  if (iter == cubes.end()){
+	    Cube c;
+	    iter = cubes.insert(iter, std::map<Eigen::Vector3i, Cube, CompVector>::value_type(p, c));
+	  }
+	  iter->second.m[1 - x][1 - y][1 - z] = true;
 	}
 
   GLfloat facecolor[] = {0, 0, 1, 0.7};
   glMaterialfv(GL_FRONT, GL_DIFFUSE, facecolor);
 
-  for (auto& p : points){
+  for (auto& iter : cubes){
     glPushMatrix();
-    glTranslated(p(0) * MC_SIZE, p(1) * MC_SIZE, p(2) * MC_SIZE);
-    glutSolidCube(MC_SIZE);
+    glTranslated(
+		 iter.first(0) * MC_SIZE,
+		 iter.first(1) * MC_SIZE,
+		 iter.first(2) * MC_SIZE
+		 );
+    iter.second.draw();
     glPopMatrix();
   }
 }
